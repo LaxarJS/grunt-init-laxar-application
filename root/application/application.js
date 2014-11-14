65dc806aee154c2c1b25804eaa432734ffc3de31
Michael Kurze <michael.kurze@aixigo.de>
2014-11-14 12:12:22 +0100

    (#16) update and simplify application.js, Gruntfile.js and .html files

--- root/application/application.js
+++ root/application/application.js
@@ -1,21 +1,19 @@
+// See https://github.com/LaxarJS/laxar/blob/master/docs/manuals/configuration.md
 window.laxar = {
    name: '{%= name %}',
    description: '{%= description %}',
 
-   theme: 'default',
-
-   locales: {
-      'default': 'en-US',
+   portal: {
+      theme: 'default',
+      useMergedCss: window.laxarMode === 'RELEASE'
    },
 
-   logThreshold: 'DEVELOP',
-
-   // relative to laxar-path-root
-   fileListings: {
-      'bower_components/laxar_uikit/themes': 'var/listing/laxar_uikit_themes.json',
-      'bower_components/laxar_uikit/controls': 'var/listing/laxar_uikit_controls.json',
-      'includes/themes': 'var/listing/includes_themes.json',
-      'includes/widgets': 'var/listing/includes_widgets.json',
-      'application/layouts': 'var/listing/application_layouts.json'
+   file_resource_provider: {
+      fileListings: {
+         'application': 'var/listing/application_resources.json',
+         'bower_components': 'var/listing/bower_components_resources.json',
+         'includes': 'var/listing/includes_resources.json'
+      },
+      useEmbedded: window.laxarMode === 'RELEASE'
    }
 };
