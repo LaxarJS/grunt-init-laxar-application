18da907b10658178de3732dd09278725d9782de9
Michael Kurze <michael.kurze@aixigo.de>
2015-06-05 11:49:30 +0200

    (#31) added missing license

--- root/demo.includes/widgets/example/my-viewer-widget/my-viewer-widget.js
+++ root/demo.includes/widgets/example/my-viewer-widget/my-viewer-widget.js
@@ -1,5 +1,5 @@
 /**
- * Copyright  {%= grunt.template.today('yyyy') %} {%= author_name %}{% if( licenses.length ) { %}
+ * Copyright {%= grunt.template.today('yyyy') %} {%= author_name %}{% if( licenses.length ) { %}
  * Released under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.{% } %}
  */
 define( [
